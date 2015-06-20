2a21f35a18052908b3d4e4ade993066ec060fcb7
Sean Fredericksen <sean.fredericksen@banno.com>
2015-06-20 15:42:08 -0500

    Adding more to rational example

--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -1,5 +1,31 @@
 
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  override def toString = n + "/" + d
+
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  override def toString = numer + "/" + denom
+   
+  def + (that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def lessThan(that: Rational) =
+    numer * that.denom < that.numer * denom
+
+  def max(that: Rational) =
+    if (lessThan(that)) that else this
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
 }
+
