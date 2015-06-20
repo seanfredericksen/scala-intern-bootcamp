318bcae4b43fd5d7e1df70668764436f325dde8f
Sean Fredericksen <sean.fredericksen@banno.com>
2015-06-19 23:32:27 -0500

    This makes more sense lol

--- src/main/scala/Employee.scala
+++ src/main/scala/Employee.scala
@@ -1,12 +1,13 @@
 import scala.util.Random
 
-class Employee(var name: String, var timeEmployedInDays: Int, employeeAge : Int) {
-  private val age = employeeAge
+class Employee(var name: String, var timeEmployedInDays: Int) {
+  private val age = Employee.getRandomAge()
 
   def getAge(): Int = age
   override def toString = name + " " + getAge() + " " + timeEmployedInDays
 }
 
 object Employee {
-  def apply(name: String, timeEmployedInDays: Int) = new Employee(name, timeEmployedInDays, Random.nextInt(82) + 18)
+  def apply(name: String, timeEmployedInDays: Int) = new Employee(name, timeEmployedInDays)
+  def getRandomAge() = Random.nextInt(82) + 18
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,8 @@ object Main {
   def main(args: Array[String]) {
     val employees = List(Employee("Sean", 900), Employee("Chris", 2))
 
+    //Do it twice to see how output changes
+    employees.foreach(println)
     employees.foreach(println)
   }
 }
