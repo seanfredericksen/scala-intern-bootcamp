2d115fb9493afac05248b8c5156c71a7b593fb2a
Sean Fredericksen <sean.fredericksen@banno.com>
2015-06-20 17:28:30 -0500

    Add for expression for string lengths

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,5 +6,16 @@ object Main {
     //Do it twice to see how output changes
     employees.foreach(println)
     employees.foreach(println)
+
+    println(forStringLengths(List("foo", "bar", "", "sean", " ", "crabshack", "", null, "hoekstra", null)))
   }
+
+  def forStringLengths(strings: List[String]) =
+    for {
+      string <- strings
+    }  yield string match {
+        case null => 0
+        case "" => -1
+        case _ => string.length
+    }
 }
